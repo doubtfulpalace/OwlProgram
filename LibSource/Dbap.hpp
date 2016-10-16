4cabac35a1fd15e966a05306d3d70e63c0592c09
pingdynasty <martin@rebel-it.com>
2016-10-16 22:31:31 +0000

    changed to use create/destroy initialisation

--- LibSource/Dbap.hpp
+++ LibSource/Dbap.hpp
@@ -7,153 +7,135 @@
 
 class DbapSource{
 public:
-	DbapSource():
-	DbapSource(6)
-	{}
+  DbapSource()
+    : DbapSource(6) {}
 
-	DbapSource(float newRolloff):
-	spread(0),
-	ys(0),
-	rSquared(0),
-	rolloff(newRolloff)
-	{
-		computeA();
-	}
+  DbapSource(float rolloff)
+    : spread(0),    
+      ys(0),
+      rSquared(0),
+      a(rolloff / (20 * log10f(2))) {}
 
-	~DbapSource(){};
+  void setSpread(float newSpread){
+    rSquared = newSpread * newSpread;
+  }
 
-	void setSpread(float newSpread){
-		rSquared = newSpread * newSpread;
-	}
+  void setPosition(float newX, float newY){
+    xs = newX;
+    ys = newY;
+  }
 
-	void setPosition(float newX, float newY){
-		xs = newX;
-		ys = newY;
-	}
+  void setPolarPosition(float angle, float r){
+    xs = r * cosf(angle / 360 * 2*M_PI);
+    ys = r * sinf(angle / 360 * 2*M_PI);
+  }
 
-	void setPositionPolar(float angle, float r){
-		xs = r * cosf(angle / 360 * 2*M_PI);
-		ys = r * sinf(angle / 360 * 2*M_PI);
-	}
-
-	void getAmplitudes(FloatArray& outputs, FloatArray speakersX, FloatArray speakersY){
-		// Assumes all the FloatArrays have the same size.
-		float sum = 0;
-		for(unsigned int n = 0; n < outputs.getSize(); ++n){
-			float xi = speakersX[n];
-			float yi = speakersY[n];
-			// use output as temporary storage 
-			// for distances d_i
-			float xDiff = xi - xs;
-			float yDiff = yi - ys;
-			outputs[n] = sqrtf(xDiff * xDiff + yDiff * yDiff + rSquared);
-			sum += 1 / powf(outputs[n], 2 * a);
-		}
-		// k = \frac{1}{sqrt{sum{1}{N} \frac{1}{d_i^(2a)}}}
-		float k = 1 / sqrtf(sum);
-		for(unsigned int n = 0; n < outputs.getSize(); ++n){
-			// v_i = \frac{k}{d_i^a}
-			outputs[n] = k / powf(outputs[n], a);
-		}
-	}
+  void getAmplitudes(FloatArray outputs, FloatArray speakersX, FloatArray speakersY){
+    // Assumes all the FloatArrays have the same size.
+    float sum = 0;
+    for(unsigned int n = 0; n < outputs.getSize(); ++n){
+      float xi = speakersX[n];
+      float yi = speakersY[n];
+      // use output as temporary storage
+      // for distances d_i
+      float xDiff = xi - xs;
+      float yDiff = yi - ys;
+      outputs[n] = sqrtf(xDiff * xDiff + yDiff * yDiff + rSquared);
+      sum += 1 / powf(outputs[n], 2 * a);
+    }
+    // k = \frac{1}{sqrt{sum{1}{N} \frac{1}{d_i^(2a)}}}
+    float k = 1 / sqrtf(sum);
+    for(unsigned int n = 0; n < outputs.getSize(); ++n){
+      // v_i = \frac{k}{d_i^a}
+      outputs[n] = k / powf(outputs[n], a);
+    }
+  }
 private:
-	float spread;
-	float xs;
-	float ys;
-	float rSquared;
-	float rolloff;
-	float a;
-	void computeA(){
-		a = rolloff / (20 * log10f(2));
-	}
+  float spread;
+  float xs;
+  float ys;
+  float rSquared;
+  float a;
 };
 
 class Dbap{
 public:
-	static Dbap* create(unsigned int sources, unsigned int speakers);
-	Dbap():
-	Dbap(0)
-	{}
+  Dbap() : sources(NULL), numSources(0) {}
+  Dbap(DbapSource* src, unsigned int num, FloatArray spkX, FloatArray spkY)
+    : sources(src), numSources(num), speakersX(spkX), speakersY(spkY)
+  {}
 
-	Dbap(unsigned int numSources):
-	sources(NULL),
-	speakersX(NULL, 0),
-	speakersY(NULL, 0)
-	{
-		setNumSources(numSources);
-		homeSources();
-	}
+  void setSources(DbapSource* src, unsigned int num){
+    sources = src;
+    numSources = num;
+  }
 
-	~Dbap(){
-		dealloc();
-	}
+  void setSpeakers(FloatArray newSpeakersX, FloatArray newSpeakersY){
+    // Argument check: pick the smallest of the two sizes to
+    // ensure they have the same size.
+    unsigned int size = min(speakersX.getSize(), speakersY.getSize());
+    speakersX = FloatArray(newSpeakersX.getData(), size);
+    speakersY = FloatArray(newSpeakersY.getData(), size);
+  }
 
-	void setNumSources(unsigned int newNumSources){
-		dealloc();
-		sources = new DbapSource[newNumSources];
-		numSources = newNumSources;
-	}
+  void setSpeakerPosition(int speaker, float x, float y){
+    if(speaker < speakersX.getSize())
+      speakersX[speaker] = x;
+    if(speaker < speakersY.getSize())
+      speakersY[speaker] = y;
+  }
+  
+  void setSourcePosition(unsigned int source, float x, float y){
+    if(source < numSources)
+      sources[source].setPosition(x, y);
+  }
 
-	void setSpeakers(FloatArray newSpeakersX, FloatArray newSpeakersY){
-		// Argument check: pick the smallest of the two sizes to
-		// ensure they have the same size.
-		unsigned int size = speakersX.getSize() < speakersY.getSize() ? 
-			speakersX.getSize() : speakersY.getSize();
-		speakersX = FloatArray(newSpeakersX.getData(), size);
-		speakersY = FloatArray(newSpeakersY.getData(), size);
-	}
+  void setSourcePolarPosition(unsigned int source, float angle, float r){
+    if(source < numSources)
+      sources[source].setPolarPosition(angle, r);
+  }
 
-	void setSourcePosition(unsigned int source, float x, float y){
-		if(source < numSources){
-			sources[source].setPosition(x, y);
-		}
-	}
+  void setSourceSpread(unsigned int source, float spread){
+    if(source < numSources)
+      sources[source].setSpread(spread);	
+  }
 
-	void setSourcePosition(FloatArray sourceX, FloatArray sourceY){
-		// TODO: maybe we should be less conservative and 
-		// skip all this parameter check / realloc
-		unsigned int size = sourceX.getSize() < sourceY.getSize() ?
-			sourceX.getSize() : sourceY.getSize();
-		if(size >= numSources){
-			setNumSources(size);
-		}
-		for(unsigned int n = 0; n < sourceX.getSize(); ++n){
-			sources[n].setPosition(sourceX[n], sourceY[n]);
-		}
-	}
+  void getAmplitudes(unsigned int source, FloatArray output){
+    // TODO: check that output.getSize() <= speakersX.getSize()
+    if(source < numSources){
+      sources[source].getAmplitudes(output, speakersX, speakersY);
+    }
+  }
 
-	void setSpread(float spread){
-		for(unsigned int n = 0; n < numSources; ++n){
-			sources[n].setSpread(spread);	
-		}
-	}
+  void homeSources(){
+    for(unsigned int n = 0; n < numSources; ++n){
+      sources[n].setPosition(0, 0);
+    }
+  }
 
-	void setSourcePositionPolar(unsigned int source, float angle, float r){
-		if(source < numSources){
-			sources[source].setPositionPolar(angle, r);
-		}
-	}
+  static Dbap* create(unsigned int sources){
+    // create a default 4-way DBAP configuration
+    FloatArray x = FloatArray::create(2);
+    FloatArray y = FloatArray::create(2);
+    x[0] = 0.0; y[0] = 0.0;
+    x[1] = 0.0; y[1] = 1.0;
+    x[2] = 1.0; y[2] = 1.0;
+    x[3] = 1.0; y[3] = 0.0;
+    DbapSource* src = new DbapSource[sources];
+    Dbap* dbap = new Dbap(src, sources, x, y);
+    return dbap;
+  }
 
-	void getAmplitudes(unsigned int source, FloatArray output){
-	// TODO: check that output.getSize() <= speakersX.getSize()
-		if(source < numSources){
-			sources[source].getAmplitudes(output, speakersX, speakersY);
-		}
-	}
-
-	void homeSources(){
-		for(unsigned int n = 0; n < numSources; ++n){
-			sources[n].setPosition(0, 0);
-		}
-	}
+  static void destroy(Dbap* dbap){
+    FloatArray::destroy(dbap->speakersX);
+    FloatArray::destroy(dbap->speakersY);
+    delete dbap->sources;
+    delete dbap;
+  }
 private:
-	void dealloc(){
-		delete [] sources;
-	}
-	DbapSource* sources;
-	unsigned int numSources;
-	FloatArray speakersX;
-	FloatArray speakersY;
-
+  DbapSource* sources;
+  unsigned int numSources;
+  FloatArray speakersX;
+  FloatArray speakersY;
 };
 #endif /*  _Dbap_h_ */
